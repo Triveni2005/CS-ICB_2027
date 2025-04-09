1e39c5915c4cb2a50a818fc9eb45f518113fcffb
Kausthubha404 <1DT24ic404@dsatm.edu.in>
2025-04-09 10:18:28 +0530

    numberguessinggame.java

--- CS-ICB/1DT24IC404/numberguessinggame.java
+++ CS-ICB/1DT24IC404/numberguessinggame.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class NumberGuessingGame {
+public class numberguessinggame {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
